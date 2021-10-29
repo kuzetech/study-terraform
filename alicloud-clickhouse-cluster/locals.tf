b3b90ceef9dd612d41d3c28f8c1ecfe9a69c550a
黄少威 <huangshaowei@xmfunny.com>
2021-10-29 10:46:48 +0800

    no message

--- alicloud-clickhouse-cluster/locals.tf
+++ alicloud-clickhouse-cluster/locals.tf
@@ -19,6 +19,44 @@ locals {
         role = "master"
       }
     }
+    master-2 = {
+      instance_availability_zone = "cn-hangzhou-b"
+      image_id                   = "m-bp17xw1100pknxkonj9y"
+      security_groups            = alicloud_security_group.default.*.id
+      vswitch_id                 = alicloud_vswitch.vsw.id
+      instance_type              = "ecs.xn4.small"
+      instance_name              = "master-2"
+      host_name                  = "master-2"
+      system_disk_category       = "cloud_efficiency"
+      data_disk_zone_id          = "cn-hangzhou-b"
+      data_disk_name             = "master-disk-2"
+      data_disk_category         = "cloud_efficiency"
+      data_disk_size             = 20
+      internet_max_bandwidth_out = 1
+      tags = {
+        app  = "clickhouse"
+        role = "master"
+      }
+    }
+    master-3 = {
+      instance_availability_zone = "cn-hangzhou-b"
+      image_id                   = "m-bp17xw1100pknxkonj9y"
+      security_groups            = alicloud_security_group.default.*.id
+      vswitch_id                 = alicloud_vswitch.vsw.id
+      instance_type              = "ecs.xn4.small"
+      instance_name              = "master-3"
+      host_name                  = "master-3"
+      system_disk_category       = "cloud_efficiency"
+      data_disk_zone_id          = "cn-hangzhou-b"
+      data_disk_name             = "master-disk-3"
+      data_disk_category         = "cloud_efficiency"
+      data_disk_size             = 20
+      internet_max_bandwidth_out = 1
+      tags = {
+        app  = "clickhouse"
+        role = "master"
+      }
+    }
   }
 
   worker_instances = {
--- alicloud-clickhouse-cluster/main.tf
+++ alicloud-clickhouse-cluster/main.tf
@@ -28,7 +28,7 @@ resource "alicloud_security_group_rule" "allow_all_tcp" {
   ip_protocol       = "tcp"
   nic_type          = "intranet"
   policy            = "accept"
-  port_range        = "22/22"
+  port_range        = "1/65533"
   priority          = 1
   security_group_id = alicloud_security_group.default.id
   cidr_ip           = "0.0.0.0/0"
